c74d12a22d8f4640131e597b66d4ad310b928ca9
karlines <karlines@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-08-13 08:44:16 +0000

    initial upload
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/lim@4 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/LIM/inst/examples/Foodweb/foodwebs.r
@@ -0,0 +1,34 @@
+###############################################
+# To read the files in this directory:
+# First set the working directory
+# setwd("    LIM/examples/Foodweb")
+###############################################
+require(LIM)
+Inverse<-Read("rigaautumn.input")
+donali <- Setup(Inverse)
+rigaweb <- Flowmatrix(donali)
+donweb <- Lsei(donali)
+Xranges (donali)
+Varranges(donali)
+Variables(donali,donweb$X)
+plotweb(rigaweb)
+
+LIMTakapoto   <- Setup("takapoto.input",verbose=FALSE)
+LIMRigaAutumn <- Setup("rigaautumn.input")
+LIMRigaSpring <- Setup("rigaspring.input",verbose=FALSE)
+LIMRigaSummer <- Setup("rigasummer.input",verbose=FALSE)
+LIMBrouageMudflat <-Setup("BrouageMudflat.input",verbose=FALSE)
+LIMEverglades <- Setup("Everglades.input",verbose=FALSE)
+LIMScheldtIntertidal <- Setup("ScheldtIntertidal.input",verbose=FALSE)
+LIMCaliforniaSediment <- Setup("CaliforniaSediment.input",verbose=FALSE)
+
+ll<-Read("LIM_example.input")
+lim<-Setup(ll)
+Ldei(lim)$X
+# all in one
+Lsei("LIM_example.input",verbose=FALSE)$X
+Ldei("LIM_example.input",verbose=FALSE)$X
+
+lim<- Setup("cosmF_.input")
+X0 <- Ldei(lim)$X
+SolXS <- xsample(E=lim$A, F=lim$B, G=lim$G, H=lim$H, sdB=1, iter=runs, type = "mirror", jmp=NULL, x0=X0, fulloutput = FALSE)
