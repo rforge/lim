7f790732fb0ac892dbeb09a095c167010cb50106
karlines <karlines@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-04-08 12:41:06 +0000

    added files
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/lim@17 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/LIM/R/Flowmatrix.R
@@ -0,0 +1,24 @@
+
+##==============================================================================
+##  Creates a matrix with the flows
+##==============================================================================
+
+Flowmatrix <- function(lim, web=NULL) {
+
+  flowmatrix <- lim$Flowmatrix
+  if (is.null(web))
+    web <- Lsei.lim(lim,parsimonious=TRUE)$X
+
+  X          <- as.vector(web)
+  Xpos       <- pmax(0.,X)
+  ii         <- which(flowmatrix >0,arr.ind=TRUE)
+  flowmatrix[ii]<-Xpos[lim$Flowmatrix[ii]]
+
+  Xneg       <- -1*pmin(0.,X)
+
+  if( sum(Xneg) > 0)
+    flowmatrix[ii[,c(2,1)]]<-flowmatrix[ii[,c(2,1)]]+Xneg[lim$Flowmatrix[ii]]
+
+  return(flowmatrix)
+
+}
